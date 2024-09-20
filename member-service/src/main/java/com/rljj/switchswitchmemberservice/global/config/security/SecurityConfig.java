184f4820183204c036d48f49e47aa54a5ff92adc
Eui9179 <outdark97@gmail.com>
2024-09-20 14:03:46 +0900

    BugFix/set-authentication-manager-in-filter

--- member-service/src/main/java/com/rljj/switchswitchmemberservice/global/config/security/SecurityConfig.java
+++ member-service/src/main/java/com/rljj/switchswitchmemberservice/global/config/security/SecurityConfig.java
@@ -3,14 +3,19 @@ package com.rljj.switchswitchmemberservice.global.config.security;
 import com.rljj.switchswitchcommon.jwt.JwtProvider;
 import com.rljj.switchswitchmemberservice.domain.auth.service.AuthLogoutHandler;
 import com.rljj.switchswitchmemberservice.domain.auth.service.AuthService;
+import com.rljj.switchswitchmemberservice.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
@@ -18,18 +23,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomAuthenticationManager customAuthenticationManager;
+    private final AuthLogoutHandler logoutHandler;
     private final JwtProvider jwtProvider;
     private final AuthService authService;
-    private final AuthLogoutHandler logoutHandler;
-    private final LoginAuthenticationFilter loginAuthenticationFilter;
-
-    @Bean
-    public LoginAuthenticationFilter loginAuthenticationFilter() {
-        LoginAuthenticationFilter filter = new LoginAuthenticationFilter(customAuthenticationManager, jwtProvider, authService);
-        filter.setFilterProcessesUrl("/api/auth/login");
-        return filter;
-    }
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, HttpSession httpSession) throws Exception {
@@ -45,7 +43,7 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/login?logout")
                         .invalidateHttpSession(true)
                         .permitAll())
-                .addFilter(loginAuthenticationFilter)
+                .addFilter(loginAuthenticationFilter())
                 .exceptionHandling(
                         exception -> exception.accessDeniedPage("/login?error=403")
                 )
@@ -54,4 +52,21 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(memberService);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new CustomAuthenticationManager(userDetailsService(), passwordEncoder);
+    }
+
+    @Bean
+    public LoginAuthenticationFilter loginAuthenticationFilter() {
+        LoginAuthenticationFilter filter = new LoginAuthenticationFilter(authenticationManager(), jwtProvider, authService);
+        filter.setFilterProcessesUrl("/api/auth/login");
+        filter.setAuthenticationManager(authenticationManager());
+        return filter;
+    }
 }
