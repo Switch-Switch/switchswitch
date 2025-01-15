d85e9862d653bb24cc2e8ef1481e410cb80b7af5
Eui9179 <outdark97@gmail.com>
2025-01-16 01:39:43 +0900

    Feature/add-jpa-config

--- chip-service/src/main/java/com/inzisoft/chipservice/global/config/jpa/JpaConfig.java
+++ chip-service/src/main/java/com/inzisoft/chipservice/global/config/jpa/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.rljj.switchswitchmemberservice.global.config.jpa;
+package com.inzisoft.chipservice.global.config.jpa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
