7d12d89c476ae85099b5d0950724ceff16f2362e
DESKTOP-93CFOT8\hjryu <h2ju1004@gmail.com>
2025-01-21 11:16:16 +0900

    feat: ChipInfo 엔티티 추가

---
+++ switchswitch-entity/src/main/java/com/rljj/switchswitchentity/chip/ChipInfo.java
@@ -0,0 +1,43 @@
+package com.rljj.switchswitchentity.chip;
+
+import com.rljj.switchswitchentity.baseentity.BaseEntity;
+import com.rljj.switchswitchentity.chip.type.ConsoleModel;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "chip_info")
+public class ChipInfo extends BaseEntity {
+    @Id
+    @Column(name = "chip_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "console_model", nullable = false)
+    private ConsoleModel consoleModel;
+
+    // 임시로 만든 builder 예시
+    @Builder
+    public ChipInfo(String name, String imageUrl, int price, ConsoleModel consoleModel) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.consoleModel = consoleModel;
+    }
+
+}
---
+++ switchswitch-entity/src/main/java/com/rljj/switchswitchentity/chip/type/ConsoleModel.java
@@ -0,0 +1,6 @@
+package com.rljj.switchswitchentity.chip.type;
+
+public enum ConsoleModel {
+    NINTENDO_SWITCH1,
+    NINTENDO_SWITCH2
+}
