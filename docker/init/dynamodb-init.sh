59dd08447ff4adf4c9b9c21d0628b62316cdc8d8
Eui9179 <outdark97@gmail.com>
2025-01-24 10:27:25 +0900

    Chore/update-dynamodb-config

--- docker/init/dynamodb-init.sh
+++ docker/init/dynamodb-init.sh
@@ -1,6 +1,11 @@
-aws dynamodb create-table\
-  --table-name ChatMessage\
-  --attribute-definitions '[{"AttributeName":"chatMessageId","AttributeType":"S"}]'\
-  --key-schema AttributeName=chatMessageId,KeyType=HASH\
-  --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1\
+aws dynamodb create-table \
+  --table-name chat_messages \
+  --attribute-definitions \
+    AttributeName=chatroom_id,AttributeType=N \
+    AttributeName=created_at,AttributeType=S \
+  --key-schema \
+    AttributeName=chatroom_id,KeyType=HASH \
+    AttributeName=created_at,KeyType=RANGE \
+  --provisioned-throughput \
+    ReadCapacityUnits=5,WriteCapacityUnits=5 \
   --endpoint-url http://dynamodb-local:8000
